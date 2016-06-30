e1748ffe9b0a7cabe721dedeeb699b04a0ca2bd2
faiq <m_faiq@live.co.uk>
2016-06-30 16:19:11 +0500

    Layout Changes

--- audit/urls.py
+++ audit/urls.py
@@ -13,11 +13,19 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include, url,patterns
 from django.contrib import admin
-import teams.views as v
+from django.conf import settings
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^main/', include('teams.urls'))
-    #url(r'^$', v.home)
 ]
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), )
+
